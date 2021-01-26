99c50d9bc25c16e76db9ab1cd5b97b17c8d6dd8e
james.warren <james.warren@digital.justice.gov.uk>
2021-01-26 21:06:56 +0000

    make me ook betta

--- run-test-hook.sh
+++ run-test-hook.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [ `grep -r '| raw' client/templates/* | grep -v 'assetSource' | wc -l` -gt 0 ]
+
+if [ `grep -r '| raw' client/templates/* | grep -v 'assetSource' | wc -l` -gt 1 ]
 then
   echo "Raw in directory"
   exit 1
 else
-  echo "No raw in directory"
   exit 0
 fi
