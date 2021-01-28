232999f03f6f6ee225d2714d473975f504558596
james.warren <james.warren@digital.justice.gov.uk>
2021-01-28 09:29:07 +0000

    wip

--- run-test-hook.sh
+++ run-test-hook.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+while getopts d: option
+do
+case "${option}"
+in
+d) DIRECTORY=${OPTARG};;
+esac
+done
 
-if [ `grep -r '| raw' client/templates/* | grep -v 'assetSource' | wc -l` -gt 1 ]
+if [ `grep -r '\(|\| |\)raw' ${DIRECTORY}/* | grep -v 'assetSource' | wc -l` -gt 1 ]
 then
-  echo "Raw in directory"
+  echo "Instances of raw in ${DIRECTORY}. Please remove"
   exit 1
 else
   exit 0
